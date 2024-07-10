7fefcd4b3d2e30ca5a885315741638d937957a98
DamColson <Damien.colson@gmail.com>
2024-07-10 11:06:15 +0200

    attribut/constructeur/getter/setter Compte/Gestionnaire/Matiere

--- factoryBack/src/main/java/factory/model/Compte.java
+++ factoryBack/src/main/java/factory/model/Compte.java
@@ -2,4 +2,54 @@ package factory.model;
 
 public class Compte {
 
+	private Integer id;
+	private String login;
+	private String password;
+	private String role;
+	
+	public Compte() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Compte(Integer id, String login, String password, String role) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
 }
--- factoryBack/src/main/java/factory/model/Gestionnaire.java
+++ factoryBack/src/main/java/factory/model/Gestionnaire.java
@@ -2,4 +2,84 @@ package factory.model;
 
 public class Gestionnaire {
 
+	private Integer id;
+	private String nom;
+	private String prenom;
+	private String telelphone;
+	private String mail;
+	private String password;
+	private Ordinateur ordinateur;
+	
+	public Gestionnaire() {
+	}
+
+	public Gestionnaire(Integer id, String nom, String prenom, String telelphone, String mail, String password,
+			Ordinateur ordinateur) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.telelphone = telelphone;
+		this.mail = mail;
+		this.password = password;
+		this.ordinateur = ordinateur;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getTelelphone() {
+		return telelphone;
+	}
+
+	public void setTelelphone(String telelphone) {
+		this.telelphone = telelphone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Ordinateur getOrdinateur() {
+		return ordinateur;
+	}
+
+	public void setOrdinateur(Ordinateur ordinateur) {
+		this.ordinateur = ordinateur;
+	}
+	
+	
 }
