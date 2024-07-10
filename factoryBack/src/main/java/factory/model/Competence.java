252c25773fb4ead54e356d24aea1f44758b11f8c
Steven LECLERC <s.leclerc076@outlook.fr>
2024-07-10 12:04:36 +0200

    Annotation Bloc Competence Formation

--- factoryBack/src/main/java/factory/model/Competence.java
+++ factoryBack/src/main/java/factory/model/Competence.java
@@ -1,7 +1,19 @@
 package factory.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "competence")
 public class Competence {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@NotBlank
 	private String nom;
 	
 	public Competence() {
